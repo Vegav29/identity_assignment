f9567287d27805c5f422c491de6377291e3d5abc
Vegav29 <123195107+Vegav29@users.noreply.github.com>
2024-04-10 03:27:13 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,3 +1,18 @@
-module.exports = (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, '..', 'index.html'));
-};
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const identifyRouter = require('./api/identify'); // Import the identify router
+const indexRouter = require('./api/index'); // Import the index router
+
+app.use(express.static(path.join(__dirname, '')));
+
+// Route handlers
+app.use('/', indexRouter);
+app.use('/api', identifyRouter); // Mount the identify router under /api
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
